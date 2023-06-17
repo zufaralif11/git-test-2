2add8454345205c2d80ea7ec4731c597545a0679
zufaralif11 <zufaralifmaulana11@gmail.com>
2023-06-17 13:27:31 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam labore, dolor officia maxime cumque facilis illum nisi inventore in! Provident in expedita ut dolor harum similique eveniet velit possimus!</p>
 </body>
 </html>
